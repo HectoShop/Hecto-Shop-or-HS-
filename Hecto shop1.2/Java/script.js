1402da5eddbbb17c0a7babf16272aff78c034b75
HectoShop <86149426+HectoShop@users.noreply.github.com>
2021-07-04 10:42:46 +0300

    Add files via upload

---
+++ Hecto shop1.2/Java/script.js
@@ -0,0 +1,38 @@
+var foo = "nume si prenume"
+console.info(foo)
+console.error(foo)
+console.log(foo)
+console.warn(foo)
+
+
+var string = 32;
+var price = 123
+var price = 124;
+
+console.log(price + price2);
+
+var booleanTrue = true;
+var booleanFalse = false;
+
+if(booleanTrue == booleanFalse)  {
+
+}
+var isnull = null;
+
+var total = price + price2;
+var price = 32;
+var price = 302;
+var price = 342;
+var price = 3243;
+var price = 3234;
+var price = 332;
+var price = 3234;
+var price = 3234;
+var price = 32;
+var price = 324;
+var price = 332;
+var price = 322;
+var price = 323;
+var price = 324;
+var price = 332;
+var price = 3332;
